65b41ba0df349d53ce6766e04ea111c9543c6b39
Taylor Petrychyn <tpetrychyn@gmail.com>
2019-12-09 21:49:08 -0600

    feat(stage-2): add bus and cartridge

--- main.go
+++ main.go
@@ -1,35 +1,46 @@
 package main
 
 import (
+	"github.com/tpetrychyn/nes-emu/bus"
 	"github.com/tpetrychyn/nes-emu/cpu"
+	"github.com/tpetrychyn/nes-emu/memory"
 	"log"
+	"time"
 )
 
 func main() {
-	testProg := []byte{0x4A, 0xA2, 0x44, 0x99}
+	b := bus.NewBus()
+
+	cart := memory.NewCartridge("nestest.nes")
+	b.AttachDevice(0xc000, cart) // 0xc000 is where nestest starts reading from
 
 	c := &cpu.Cpu{
-		A: 0x1,
+		PC: 0xc000,
 	}
 
 	for {
-		in := testProg[c.PC]
+		in := b.Read(c.PC)
 
 		log.Printf("PC:%4X In:%2X A:%2X X:%2X Y:%2X - SR: %2X", c.PC, in, c.A, c.X, c.Y, c.SR)
 
 		switch in {
-		case 0x99:
+		case 0xFF:
 			return
 		case 0x4A: // LSR Accumulator - shift all bits right 1 pos
 			c.SetStatus(1 << 0, (c.A&1) == 1)
 			c.A >>= 1
 			c.PC ++
 		case 0xA2: // LDX Immediate - store immediate value in X
-			c.X = testProg[c.PC+1]
+			c.X = b.Read(c.PC+1)
 			c.PC += 2
+		case 0x4C: // jmp absolute
+			addr := uint16(b.Read(c.PC+2)) << 8 | uint16(b.Read(c.PC+1))
+			c.PC = addr
 		default:
 			c.PC++
 		}
+
+		<- time.After(1 * time.Second)
 	}
 }
 
